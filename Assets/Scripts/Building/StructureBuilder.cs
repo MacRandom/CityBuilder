af013601ed7015c89eb66412c711dd7fe34f932c
MacRandom <macrandomstudio@gmail.com>
2021-02-18 17:59:07 +0700

    Реализовал выбор здания для строительства.

--- Assets/Scripts/Building/StructureBuilder.cs
+++ Assets/Scripts/Building/StructureBuilder.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
+using CityBuilder.Data;
 
 namespace CityBuilder.BuildingSystem
 {
     public class StructureBuilder : MonoBehaviour
     {
-        [SerializeField]
         private GameObject _buildingPrefab;
-        [SerializeField]
-        private GameObject _placeholderPrefab;
         private GameObject _placeholder;
         private float _xMin = -20f;
         private float _xMax = 19f;
@@ -15,36 +13,42 @@ namespace CityBuilder.BuildingSystem
         private float _zMax = 19f;
         private BuildingPossibility _buildingPossibility;
 
-        private void OnEnable()
+        public void BuildingStart(BuildingData buildingData)
         {
-            _placeholder = Instantiate(_placeholderPrefab);
-            _buildingPossibility = _placeholder.GetComponentInChildren<BuildingPossibility>();
-        }
+            if (_placeholder != null)
+                Destroy(_placeholder);
 
-        private void OnDisable()
-        {
-            Destroy(_placeholder);
+            _placeholder = Instantiate(buildingData.PlaceholderPrefab);
+            _buildingPossibility = _placeholder.GetComponentInChildren<BuildingPossibility>();
+            _buildingPrefab = buildingData.BuildingPrefab;
         }
 
         private void FixedUpdate()
         {
-            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (_placeholder != null)
+            {
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(ray, out RaycastHit hit))
-                if (hit.transform.CompareTag("Ground"))
-                {
-                    _placeholder.transform.position = new Vector3(
-                        Mathf.Clamp(Mathf.Round(hit.point.x), _xMin, _xMax),
-                        0,
-                        Mathf.Clamp(Mathf.Round(hit.point.z), _zMin, _zMax));
-                }
+                if (Physics.Raycast(ray, out RaycastHit hit))
+                    if (hit.transform.CompareTag("Ground"))
+                    {
+                        _placeholder.transform.position = new Vector3(
+                            Mathf.Clamp(Mathf.Round(hit.point.x), _xMin, _xMax),
+                            0,
+                            Mathf.Clamp(Mathf.Round(hit.point.z), _zMin, _zMax));
+                    }
+            }
         }
 
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-                if (_buildingPossibility.IsPossible)
-                    Instantiate(_buildingPrefab, _placeholder.transform.position, Quaternion.identity);
+            if (_placeholder != null && _buildingPrefab != null)
+                if (Input.GetMouseButtonDown(0))
+                    if (_buildingPossibility.IsPossible)
+                    {
+                        Instantiate(_buildingPrefab, _placeholder.transform.position, Quaternion.identity);
+                        Destroy(_placeholder);
+                    }
         }
     }
 }
---
+++ Assets/Scripts/Data/BuildingData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CityBuilder.Data
+{
+    [CreateAssetMenu(fileName = "Building Data", menuName ="Game Data/Building Data")]
+    public class BuildingData : ScriptableObject
+    {
+        public GameObject BuildingPrefab;
+        public GameObject PlaceholderPrefab;
+    }
+}
