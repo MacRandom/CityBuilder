0514b9ae9438baf222699e72f35258f12f7b859b
MacRandom <macrandomstudio@gmail.com>
2021-02-20 14:29:23 +0700

    Добавил объекты ресурсов и UI для них.

---
+++ Assets/Scripts/Game/PlayerResource.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+namespace CityBuilder.Game
+{
+    public class PlayerResource : MonoBehaviour
+    {
+        [SerializeField]
+        private string _name;
+        [SerializeField]
+        private int _current;
+        [SerializeField]
+        private int _max;
+
+        public event EventHandler<int> CurrentValueChanged;
+        public event EventHandler<int> MaxValueChanged;
+
+        public string Name { get => _name; }
+
+        public int Current
+        {
+            get => _current;
+            private set
+            {
+                if (_current != value)
+                {
+                    _current = value;
+                    CurrentValueChanged?.Invoke(this, _current);
+                }
+            }
+        }
+
+        public int Max
+        {
+            get => _max;
+            private set
+            {
+                if (_max != value)
+                {
+                    _max = value;
+                    MaxValueChanged?.Invoke(this, _max);
+                }
+            }
+        }
+
+        public void OnGather(object sender, int value) => Current += value;
+        public void OnSpent(object sender, int value) => Current -= value;
+        public void OnVaultBuilded(object sender, int value) => Max += value;
+    }
+}
---
+++ Assets/Scripts/Game/ResourceUI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace CityBuilder.Game
+{
+    public class ResourceUI : MonoBehaviour
+    {
+        [SerializeField]
+        private PlayerResource _resource;
+        [SerializeField]
+        private Text _nameText;
+        [SerializeField]
+        private Text _currentText;
+        [SerializeField]
+        private Text _maxText;
+
+
+        private void Start()
+        {
+            _nameText.text = string.Concat(_resource.Name, ":");
+            _currentText.text = _resource.Current.ToString();
+            _maxText.text = _resource.Max.ToString();
+
+            _resource.CurrentValueChanged += OnCurrentValueChanged;
+            _resource.MaxValueChanged += OnMaxValueChanged;
+        }
+
+        private void OnMaxValueChanged(object sender, int e)
+        {
+            _maxText.text = e.ToString();
+        }
+
+        private void OnCurrentValueChanged(object sender, int e)
+        {
+            _currentText.text = e.ToString();
+        }
+    }
+}
